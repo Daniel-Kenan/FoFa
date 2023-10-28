71794333f9384620c50881be925ba4ec2fda3c0d
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2023-10-28 07:14:39 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set EJS as the view engine
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+// Define a simple route
+app.get('/', (req, res) => {
+    // Render the "index.ejs" template
+    res.render("index");
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
