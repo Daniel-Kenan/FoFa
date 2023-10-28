1d52781e97a8c136073a61f5d31b13070ec2603b
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2023-10-28 22:51:02 +0200

    commit msg

--- server.js
+++ server.js
@@ -1,13 +1,52 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5500;
 const bodyParser = require('body-parser');
 const users = [];
+const session = require('express-session');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
     app.set('view engine', 'ejs');
 
-// Define a simple route
+
+
+app.use(
+        session({
+          secret: 'your_secret_key', // Use a secret key to sign the session ID cookie
+          resave: false,
+          saveUninitialized: true,
+        })
+      );
+
+app.post('/signin', (req, res) => {
+        const { email, password } = req.body;
+      
+        // Check if the provided email and password match a user's credentials
+        const user = users.find((user) => user.email === email && user.password === password);
+      
+        if (user) {
+          req.session.user = user;
+          res.redirect("/profile")
+        } else {
+          res.send('Invalid email or password');
+        }
+      });
+app.post('/signup', (req, res) => {
+        const { name, email, password } = req.body;
+    
+        // Check if the email is already registered
+        const existingUser = users.find((user) => user.email === email);
+        if (existingUser) {
+            res.send('Email already registered');
+        } else {
+            // Store the data in the users array
+            users.push({ name, email, password });
+            res.redirect("/signin")
+        }
+    });
+
+
+    // Define a simple route
 app.get('/', (req, res) => {
     // Render the "index.ejs" template
     res.render("index");
@@ -17,7 +56,7 @@ app.get('/signup', (req, res) => {
   
     res.render("sign-up");
 });
-
+-+
 app.get('/signin', (req, res) => {
     res.render("sign-in");
 });
@@ -29,11 +68,17 @@ app.get('/pages/profile.html', (req, res) => {
 });
 
 app.get('/profile', (req, res) => {
-    res.render("profile");
+    if (req.session.user) {
+        // User is authenticated, show their profile
+        res.render('profile',{user:req.session.user});
+    } else {
+        // User is not authenticated, redirect to the sign-in page
+        res.redirect('/signin');
+    }
 });
 
 app.get('/dashboard', (req, res) => {
-    res.render("dashboard");
+    res.render("dashboard",{user:req.session.user});
 });
 
 
